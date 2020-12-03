855300a6a8f39605d7d7e5b90ec983d31e788e76
Alexandra Waldau <alew@itu.dk>
2020-12-03 15:47:53 +0100

    restructure code v2

--- envrmt/src/Components/Dashboard/Pop-ups/feedback.js
+++ envrmt/src/Components/Dashboard/Pop-ups/feedback.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Rating from "react-rating";
 
 import "./feedback.css";
-import { Co2Avoided } from "../../Challenge/challenge-status";
+import { Co2Avoided } from "../Challenge/challenge-status";
 import { CategoryText } from "./challenge-detail";
 
 //icon imports
