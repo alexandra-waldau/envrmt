ab5dc66e0414aea8adda53e5bf268019cd819764
Marek Soos <masoo@itu.dk>
2020-12-19 22:54:25 +0100

    popup done

--- envrmt/src/Components/Dashboard/Pop-ups/feedback.js
+++ envrmt/src/Components/Dashboard/Pop-ups/feedback.js
@@ -45,7 +45,7 @@ const ChallengeCompletion = (props) => {
 				<p className="challenge-name-detail">{props.title}</p>
 				<p className="challenge-description-detail">{props.description}</p>
 				<EarthRating score={props.score} />
-				<button className="done-button" onClick={props.next}>
+				<button className="challenge-button" onClick={props.next}>
 					Done
 				</button>
 				<p className="failed-challenge" onClick={props.next}>
@@ -74,7 +74,7 @@ const PerformanceFeedback = (props) => {
 						id={props.id}
 					/>
 				</div>
-				<button className="done-button" onClick={props.toggle}>
+				<button className="challenge-button" onClick={props.toggle}>
 					Done
 				</button>
 			</div>
